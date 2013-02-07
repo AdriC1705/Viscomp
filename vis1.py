812329d84f485c7456368755b0fef52c97c75af2
AdriC1705 <adrianaa.1705@gmail.com>
2013-02-06 18:33:05 -0600

    escala de grises

--- vis1.py
+++ vis1.py
@@ -1,24 +1,43 @@
-from PIL import Image #esto para trabajar con imagenes
+import Image #esto para trabajar con imagenes
 import sys
 import pygame
 
 
 #definimos constantes para la ventana :)
 
-ancho = 600
-alto = 600
+#cargamos y abrimos imagen
+def imagen():
+    img = Image.open("patitos.jpg")
+    ancho,alto = img.size
+    img = eg(img,ancho,alto)
+    return img, ancho, alto
+    
+def eg(img,ancho,alto):
+    pixeles = img.load()
+    imageng = 'escgrises.bmp'
+    for i in range (ancho):
+        for j in range(alto):
+            (r,g,b)= img.getpixel((i,j))
+            prom = int((r+g+b)/3)
+            pixeles[i,j] = (prom,prom,prom)
+    img.save(imageng)
+    return imageng
 
 def main ():
     pygame.init()
+    #pygame.display.set_caption("Ventana")
+    r,ancho,alto = imagen()
+    screen = pygame.display.set_mode((ancho,alto))
     pygame.display.set_caption("Ventana")
-    screen = pygame.display.set_mode((ancho,alto), 0, 32)
-
+    im = pygame.image.load(r)
     while True:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 sys.exit(0)
+        screen.blit(im,(0,0))
+        pygame.display.update()
+    return 0
 
-if __name__ == '__main__':
-    main()
+main()
 
 
