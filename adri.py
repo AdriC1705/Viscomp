99c289929b78ab0200a6012eb113345e78538ff7
AdriC1705 <adrianaa.1705@gmail.com>
2013-02-19 07:34:27 -0600

    centros de masa

--- adri.py
+++ adri.py
@@ -4,6 +4,7 @@ import pygame
 import math
 from time import *
 import random
+import ImageDraw
 
 #definimos
 
@@ -12,8 +13,8 @@ maximo = 200
 
 #cargamos y abrimos imagen
 def imagen():
-    img = Image.open("sakura.jpg")
-    img2= Image.open("sakura.jpg")
+    img = Image.open("figs.jpg")
+    img2= Image.open("figs.jpg")
     ancho,alto = img.size
     img = eg(img,ancho,alto,img2)
     return img, ancho, alto
@@ -161,6 +162,10 @@ def binarizacion(img,ancho,alto):
 
 def formas(img,ancho,alto):
     pixel= img.load()
+    cntdr = []
+    colors=[]
+    porcentajes=[]
+    cm=[] #cm = centros de masa
     for i in range(ancho):
         for j in range(alto):
             if pixel[i,j]== (0,0,0):
@@ -168,10 +173,42 @@ def formas(img,ancho,alto):
                 b=random.randint(0,255)
                 c=random.randint(0,255)
                 (r,g,b)= (a,b,c)
-                bfs(img,ancho,alto,(r,g,b),(i,j)) #se llama a la funcion bfs
+                #bfs(img,ancho,alto,(r,g,b),(i,j)) #se llama a la funcion bfs
+                cont,color,c1,c2 = bfs(img,ancho,alto,(r,g,b),(i,j))
+                por=(cont/float(ancho*alto))*100
+                if por>.1:
+                    cntdr.append(cont)
+                    colors.append(color)
+                    porcentajes.append(por)
+                #print 'ya pase'
+                try:
+#c1 =centro 1 y c2 = centro 2
+                    cm.append((sum(c1)/float(len(c1)),sum(c2)/float(len(c2))))
+                except:
+                    pass
+#print 'sali'
+    m = cntdr.index(max(cntdr))
+    n = colors[m]
+    print m
+    print n
+    for i in range(ancho):
+        for j in range(alto):
+            if pixel[i,j] == n:
+                pixel[i,j]=(160,160,160)
+                #print 'pase por aki'
+    #porcentajes
+    #porcentaje = (float(num)/(ancho*alto))*100
+
+    draw=ImageDraw.Draw(img)
+    for i,Z in enumerate(cm):
+        draw.ellipse((Z[0]-2,Z[1]-2,Z[0]+2,Z[1]+2),fill=(0,0,0))# dibujar elipse 
+    img =img.save('cm.jpg')
 
 def bfs(img,ancho,alto,color,posa):
     pixel= img.load()
+    cont=0
+    cen1=[]
+    cen2=[]
     cola=[] #creamos la cola
     cola.append(posa) #posicion actual se agrega a la cola
     inicio = pixel[posa]
@@ -181,35 +218,46 @@ def bfs(img,ancho,alto,color,posa):
         if (posa == inicio or posa ==color):
             try:
                 if(pixel[i-1,j]):
-                       if(pixel[i-1,j]==inicio):
-                           pixel[i-1,j] = color
-                           cola.append((i-1,j))
+                    if(pixel[i-1,j]==inicio):
+                        pixel[i-1,j] = color
+                        cola.append((i-1,j))
+                        cont+=1 # contador para encontrar los pixeles ke van en gris
+                        cen1.append((i-1))
+                        cen2.append((j))
             except:
                 pass
             try:
                 if(pixel[i+1,j]):
-                       if(pixel[i+1,j]==inicio):
-                           pixel[i+1,j] = color
-                           cola.append((i+1,j))
+                    if(pixel[i+1,j]==inicio):
+                        pixel[i+1,j] = color
+                        cola.append((i+1,j))
+                        cont+=1
+                        cen1.append((i+1))
+                        cen2.append((j))
             except:
                 pass
             try:
                 if(pixel[i,j-1]):
-                       if(pixel[i,j-1]==inicio):
-                           pixel[i,j-1] = color
-                           cola.append((i,j-1))
+                    if(pixel[i,j-1]==inicio):
+                        pixel[i,j-1] = color
+                        cola.append((i,j-1))
+                        cont+=1
+                        cen1.append((i))
+                        cen2.append((j-1))
             except:
                 pass
             try:
                 if(pixel[i,j+1]):
-                       if(pixel[i,j+1]==inicio):
-                           pixel[i,j+1] = color
-                           cola.append((i,j+1))
+                    if(pixel[i,j+1]==inicio):
+                        pixel[i,j+1] = color
+                        cola.append((i,j+1))
+                        cont+=1
+                        cen1.append((i))
+                        cen2.append((j+1))
             except:
                 pass
     img = img.save('forms.jpg')
-
-#def centros():
+    return cont,color,cen1,cen2
 
 
 def main ():
